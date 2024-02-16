fe96800e6a62cb26b9a772da97de785627bd207c
satece <satklu@gmail.com>
2024-02-16 14:58:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ App.get('/product', async (req, res) => {
     try {
         const data = {
             name: String(req.query.name),
-            quantity: String(req.query.quantity)
+            quantity: Number(req.query.quantity)
         };
 
         const product = await Product.create(data);
